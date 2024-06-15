d9bd83888d40abcde396d86ed5f0f6066963711d
magicalturtle430 <144983455+magicalturtle430@users.noreply.github.com>
2024-06-15 20:40:06 +0000

    SiteProps info about myself

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Marko Paloski",
+  title: "System Engineer",
+  email: "marko.paloski@studentambassadors.com",
+  gitHub: "magicalturtle430",
+  linkedIn: "Marko Paloski",
+  twitter: "mpaloski",
 };
 
 const primaryColor = "#4E567E";
